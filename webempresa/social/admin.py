611a743eb71a10e360f67de6680318457bfb2b0e
suss-gmez <jesusgdea@gmail.com>
2021-12-16 22:18:59 -0400

    actualizando

--- webempresa/social/admin.py
+++ webempresa/social/admin.py
@@ -5,11 +5,10 @@ from .models import Link
 class LinkAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
 
-    def get_readonly_fields(self, request, obj: None):
+    """def get_readonly_fields(self, request, obj: None):
         if request.user.groups.filter(name="Personal").exists():
             return ('key', 'name')
         else:
-            ('created', 'updated')
-
+            ('created', 'updated')"""
 
 admin.site.register(Link, LinkAdmin)
